77c3048c55b13e088c78492812a503f79f245f94
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-25 16:41:22 +0200

    update

--- script.js
+++ script.js
@@ -10,8 +10,23 @@
 // Quanti millisecondi mi separano da domani alle 9:30?
 // Esiste un oggetto JS in grado di gestire le date?
 // Esistono dei metodi per trasformare una data in millisecondi?
-const countDownGoal = new Date('May 26, 2023 09:30:00').getTime();  // timestamp return
-const realTime = new Date().getTime(); // timestamp return
-const timeFrame = countDownGoal - realTime;
-console.log(timeFrame);
+ // timestamp return
+ // timestamp return
 
+
+
+const clock = setInterval(function(){
+const countDownGoal = new Date('May 26, 2023 09:30:00').getTime();     
+const realTime = new Date().getTime();   
+const timeFrame = countDownGoal - realTime;
+const milliseconds = timeFrame * 1000; 
+const seconds = Math.floor((milliseconds / 1000) % 60);
+const minutes = (seconds / 60) % 60;
+const hours = Math.floor((minutes / 60) % 24) ;
+const days = Math.floor((hours / 24) % 24);
+
+const countDownElement = document.getElementById('countdown');
+
+countDownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+clearInterval();
+}, 1000)

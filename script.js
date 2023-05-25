3e7e35609126f2aeaae78baf338dd63a6a7aebb9
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-25 15:29:36 +0200

    initialized main variables

---
+++ script.js
@@ -0,0 +1,17 @@
+// Traccia
+// Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+// Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+// Consigli
+// Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
+// Da quante ore è composto un giorno?
+// Da quanti minuti è composta un'ora?
+// Da quanti secondi è composto un minuto?
+// Da quanti millisecondi è composto un secondo?
+// Quanti millisecondi mi separano da domani alle 9:30?
+// Esiste un oggetto JS in grado di gestire le date?
+// Esistono dei metodi per trasformare una data in millisecondi?
+const countDownGoal = new Date('May 26, 2023 09:30:00').getTime();  // timestamp return
+const realTime = new Date().getTime(); // timestamp return
+const timeFrame = countDownGoal - realTime;
+console.log(timeFrame);
+
